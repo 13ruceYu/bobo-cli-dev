35ecdbdb820d016faa38c4ec2e50e5caf7b0848a
13ruceYu <bruceyuhongbo@gmail.com>
2022-11-17 23:54:47 +0800

    feat: commander init

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -11,11 +11,14 @@ const userHome = require('user-home');
 const pathExists = require('path-exists');
 const minimist = require('minimist')
 const dotEnv = require('dotenv')
+const commander = require('commander')
 
 const pkg = require('../package.json');
 const constants = require('./const');
 
-let args, config;
+let args;
+
+const program = new commander.Command()
 
 async function core () {
   try {
@@ -23,19 +26,60 @@ async function core () {
     checkNodeVersion();
     checkRoot();
     checkUserHome();
-    checkInputArgs();
+    // checkInputArgs();
     checkEnv();
     await checkGlobalUpdate();
+    registerCommand()
   } catch (e) {
     log.error(e.message)
   }
 }
 
+function registerCommand () {
+  program
+    .name(Object.keys(pkg.bin)[0])
+    .usage('<command> [options]')
+    .version(pkg.version)
+    .option('-d, --debug', '是否开启调试模式', false)
+
+  program
+    .command('init [projectName]')
+    .option('-f, --force', '是否强制初始化项目')
+    .action((projectName, cmdObj) => {
+      console.log('init', projectName, cmdObj);
+    })
+
+  program.on('option:debug', function () {
+    if (this.opts().debug) {
+      process.env.LOG_LEVEL = 'verbose'
+    } else {
+      process.env.LOG_LEVEL = 'info'
+    }
+    log.level = process.env.LOG_LEVEL
+    log.verbose('test')
+  })
+
+  // 监听未知命令
+  program.on('command:*', function (obj) {
+    const availableCommand = program.command.map(cmd => cmd.name());
+    console.log(colors.red('未知命令：' + obj[0]))
+    if (availableCommand.length > 0) {
+      console.log(colors.red('可用命令：' + availableCommand.join(', ')))
+    }
+  })
+
+  if (program.argv && program.args.length < 1) {
+    program.outputHelp()
+    console.log()
+  }
+
+  program.parse(process.argv)
+}
+
 async function checkGlobalUpdate () {
   // 1. 获取当前版本号和模块名
   const currentVersion = pkg.version;
-  // const npmName = pkg.name;
-  const npmName = 'lodash';
+  const npmName = pkg.name;
   // 2. 调用 NPM api 获取所有版本号
   const { getNpmSemverVersion } = require('@bobo-cli-dev/get-npm-info')
   const lastVersion = await getNpmSemverVersion(currentVersion, npmName)
@@ -51,7 +95,7 @@ async function checkEnv () {
   const dotEnvPath = path.resolve(userHome, '.env')
   const flag = await pathExists(dotEnvPath)
   if (flag) {
-    config = dotEnv.config({
+    dotEnv.config({
       path: dotEnvPath
     })
   }
